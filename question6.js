97c08ec32c108d2e8a4ec6a2b5d5ac286efacf47
Mark Buchthal <mark.buchthal@gmail.com>
2015-06-08 18:29:29 -0700

    add prevent default

--- question6.js
+++ question6.js
@@ -11,14 +11,16 @@ function returnRandomString(arrayOfStrings) {
   var randomString = Math.floor(Math.random() * arrayOfStrings.length);
   if (typeof arrayOfStrings[randomString] === 'string') {
     stringResult = arrayOfStrings[randomString];
-    // return arrayOfStrings[randomString];
+    answer.innerHTML = stringResult;
   } else {
     console.log('This is not a string!');
   }
 }
 
-buttonOne.addEventListener('click', returnRandomString(arrayOne));
+buttonOne.addEventListener('click', function(e) {
+  e.preventDefault();
+  returnRandomString(arrayOne);
+});
 
-answer.innerHTML = stringResult;
 
 })();
